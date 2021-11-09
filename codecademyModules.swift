c847164141c31e39ee96952de0005f15977818a2
R. Cem Kahveci <88493840+khvci@users.noreply.github.com>
2021-11-09 19:24:06 +0300

    dictionaries added to codecademyModules.swift

--- codecademyModules.swift
+++ codecademyModules.swift
@@ -201,3 +201,142 @@ var fruitEmojis: Set = ["🍇", "🍎", "🍒"]
 
 var veggieEmojis = foodEmojis.subtracting(fruitEmojis)
 print(veggieEmojis)
+
+// Creating an Empty Dictionary
+
+var emptyLiteral: [String: Int] = [:]
+var emptyInitializer = [String: Bool]()
+
+// Creating a Dictionary Literal
+
+var roleModels: [String: String] = [
+  "Mr. Rogers": "Fred McFeely Rogers",
+  "The Crocodile Hunter": "Stephen Robert Irwin",
+  "Bill Nye the Science Guy": "William Sanford Nye"
+]
+print(roleModels)
+
+// Type Inference
+
+var movieYears = [
+  "Finding Nemo": 2003,
+  "Toy Story": 1995
+]
+print(movieYears)
+
+// Adding Elements
+
+var teaSteepingTemperature = [
+  "Black": 212,
+  "Oolong": 185,
+  "White": 185,
+]
+teaSteepingTemperature["Green"] = 185
+teaSteepingTemperature["Rooibos"] = 212
+print(teaSteepingTemperature)
+
+// updating elements
+
+var abbreviations = [
+  "LOL": "Laboring Over Latkes",
+  "LMK": "Let Me Know",
+  "BRB": "Bringing Radishes Back",
+  "GJOYC": "Great Job On Your Code"
+]
+
+// Add your code below 🆘
+
+abbreviations["LOL"] = "Laugh Out Loud"
+
+abbreviations.updateValue("Be Right Back", forKey: "BRB")
+
+print(abbreviations)
+
+// Removing Elements
+
+ar rainbowHex = [
+  "red": "#ff0000",
+  "pink": "#ffc0cb",
+  "yellow": "#ffff00",
+  "maroon": "#800000",
+  "green":  "#00ff00",
+  "blue": "#0000ff",
+  "violet": "#ee82ee"
+]
+
+// Write your code below 🌈
+
+rainbowHex["maroon"] = nil
+
+rainbowHex.removeValue(forKey: "pink")
+
+rainbowHex.removeAll()
+
+print(rainbowHex)
+
+// Inspecting a Dictionary
+
+var numberOfSides = [
+  "triangle": 3,
+  "square": 4,
+  "rectangle": 4,
+  "decagon": 10,
+  "triacontagon": 30
+]
+
+if numberOfSides.isEmpty {
+  print("This dictionary has no elements in it.")
+} else {
+  print(numberOfSides.count)
+}
+
+// Accessing Values
+
+var flowerNames = [
+  "Lily": "Lilium",                
+  "Sunflower": "Helianthus", 
+  "Orchid": "Orchidaceae", 
+  "Daffodil": "Narcissus"
+]
+
+var sunflowerScientific = flowerNames["Sunflower"]!
+
+print(sunflowerScientific)
+
+if let lilyScientific = flowerNames["Lily"] {
+  print("A lily is referred to as a \(lilyScientific) in the science community.")
+}
+
+// Iterating through a Dictionary
+
+var mythology = [
+  "Zeus": "Jupiter",
+  "Athena": "Minerva", 
+  "Poseidon": "Neptune",
+  "Demeter": "Ceres"]
+
+// Write your code below 🏛️ 
+for  (greekName, romanName) in mythology {
+  print("\(greekName) is also known as \(romanName)")
+}
+
+// Using .keys and .values
+
+var lemonadeStand = [
+  "April": 8.50,
+  "May": 12.75,
+  "June": 22.50,
+  "July": 38.25, 
+  "August": 32.50,
+  "September": 11.50
+]
+
+var total: Double = 0.0
+
+// Add your code below 🍋
+for monthlyProfit in lemonadeStand.values {
+  total += monthlyProfit
+}
+print("Total profits are \(total)")
+
+// 
