0d61c14b6aa16aef4f0f20555b19ebcb3be5f9d9
R. Cem Kahveci <88493840+khvci@users.noreply.github.com>
2021-11-15 01:05:14 +0300

    classes added to codecademyModules.swift

--- codecademyModules.swift
+++ codecademyModules.swift
@@ -527,12 +527,13 @@ struct Band {
     }
   }
   
-  mutating func changeGenre(newGenre: String) -> String {
+  mutating func changeGenre(newGenre: String) -> String 
+    {
     self.genre = newGenre
     return self.genre
-  } 
+    } 
   
-}
+  }
 
 var journey = Band(genre: "rock", members: 5, isActive: true)
 
@@ -542,15 +543,17 @@ var bts: Band = Band(genre: "kpop", members: 7, isActive: true)
 
 // Structures are Value Types
 
-struct Finch {
+struct Finch 
+  {
   var lengthInCm: Double
   var nestLocation: String
 
-  init(lengthInCm: Double, nestLocation: String) {
+  init(lengthInCm: Double, nestLocation: String) 
+    {
     self.lengthInCm = lengthInCm
     self.nestLocation = nestLocation
+    }
   }
-}
 
 var groundFinch = Finch(lengthInCm: 15.0, nestLocation: "Bush")
 
@@ -558,3 +561,117 @@ var cactusFinch = groundFinch
 cactusFinch.nestLocation = "Cactus"
 print(cactusFinch.nestLocation)
 print(groundFinch.nestLocation)
+
+// classes
+
+class Restaurant 
+  {
+  var name = ""
+  var type = [""]
+  var rating = 0.0
+  var delivery = false
+  }
+
+var bobsBurgers = Restaurant()
+
+bobsBurgers.name = "Bob's Burgers"
+bobsBurgers.type = ["Burgers", "Fast Food"]
+bobsBurgers.rating = 3.5
+bobsBurgers.delivery = true
+
+print(bobsBurgers.name)
+print(bobsBurgers.type)
+print(bobsBurgers.rating)
+print(bobsBurgers.delivery)
+
+// The Init Method
+
+class Restaurant2 
+  { 
+  var name = ""
+  var type = [""]
+  var rating = 0.0
+  var delivery = false
+
+  init(name: String, type: [String], rating: Double, delivery: Bool) 
+    {
+    self.name = name
+    self.type = type
+    self.rating = rating
+    self.delivery = delivery
+    }
+  }
+
+var laRatatouille = Restaurant2(name: "La Ratatouille", type: ["French"], rating: 4.7, delivery: false)
+
+print(laRatatouille.name)
+print(laRatatouille.type)
+print(laRatatouille.rating)
+print(laRatatouille.delivery)
+
+// Inheritence and Overriding Methods
+
+class Order 
+  {
+  var items = [""]
+  var subtotal = 0.0
+  var tip = 0.0
+  var total = 0.0
+
+  func printReceipt() 
+    {
+    print("Items:     \(items)")
+    print("Subtotal:  $\(subtotal)")
+    print("Tip:       $\(tip)")
+    print("Total:     $\(total)")
+    }
+  }
+
+class DeliveryOrder: Order 
+  {
+  var deliveryFee = 2.0
+  override func printReceipt()
+    {
+    print("Items:     \(items)")
+    print("Subtotal:  $\(subtotal)")
+    print("Tip:       $\(tip)")
+    print("Delivery:  $\(deliveryFee)")
+    print("Total:     $\(total)")
+    }  
+  }
+
+var order2 = DeliveryOrder()
+
+order2.items = ["Ramen", "Diet Coke"]
+order2.subtotal = 14.69
+order2.tip = 2.00
+order2.deliveryFee = 3.00
+order2.total = 19.69
+
+order2.printReceipt()
+
+// Classes are Reference Types (unlike value type-structures)
+
+class Order 
+  {
+  var items = [""]
+  var subtotal = 0.0
+  var tip = 0.0
+  var total = 0.0
+  
+  init(items: [String], subtotal: Double, tip: Double, total: Double) 
+    {
+    self.items = items
+    self.subtotal = subtotal
+    self.tip = tip
+    self.total = total
+    }
+  }
+
+var order1 = Order(items: ["Chili Fries", "Lemonade"], subtotal: 8.75, tip: 2.0, total: 12.75)
+
+var order8 = order1
+order8.total = 0.0
+
+print(order1.total) //prints 0.0
+print(order8.total) //also prints 0.0
