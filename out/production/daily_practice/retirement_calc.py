6f2bb4bbfc1a6cfe2825594c0021a6c49558676d
R. Cem Kahveci <88493840+khvci@users.noreply.github.com>
2022-01-03 01:09:40 +0300

    Delete out/production/daily_practice directory

--- out/production/daily_practice/retirement_calc.py
+++
@@ -1,8 +0,0 @@
-name = input("What is your name?\n")
-birth = int(input("What is your year of birth?\n"))
-year = 2021
-age = year - birth
-retirement = int(input("What is retirement age in your country?\n"))
-years_to_work = retirement - age
-ret_year = year + years_to_work
-print(f"{name}, you are {age} years old. That means you need to work {years_to_work} more years to retire, considering that the retirement age is {retirement}. So, you will be retired in {ret_year}.")
